7c1babc0da4eb4c15953aaf07b84b7c6a0f95768
msllagas <mndyllagas@gmail.com>
2025-05-04 14:38:10 +0800

    feat(ui): integrate ShadCN components for UI styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <>
-      <h1 className="my-2 text-center text-2xl font-semibold">
-        Welcome to <span className="text-blue-500">HireLog</span>
-      </h1>
+      <div className="flex flex-col items-center justify-center py-2">
+        <h1 className="my-2 text-center text-2xl font-semibold">
+          Welcome to <span className="text-blue-500">HireLog</span>
+        </h1>
+
+        <div className="space-x-2">
+          <Button>Login</Button>
+          <Button>Sign Up</Button>
+        </div>
+      </div>
     </>
   );
 }
