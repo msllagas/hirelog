ce3878b8a732b9f545600491a1f69f12a8777036
msllagas <mndyllagas@gmail.com>
2025-05-10 20:25:36 +0800

    style(home): update button classes for styling

--- src/pages/home/Welcome.tsx
+++ src/pages/home/Welcome.tsx
@@ -6,14 +6,20 @@ export function Welcome() {
     <>
       <div className="flex flex-col items-center justify-center py-2">
         <h1 className="my-2 text-center text-2xl font-semibold">
-          Welcome to <span className="text-blue-500">HireLog</span>
+          Welcome to{" "}
+          <span className="bg-linear-to-r from-blue-500 to-cyan-500 bg-clip-text text-transparent font-bold">
+            HireLog
+          </span>
         </h1>
 
         <div className="space-x-2">
-          <Button asChild>
+          <Button variant="outline" asChild>
             <NavLink to="/login">Login</NavLink>
           </Button>
-          <Button asChild>
+          <Button
+            className="bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 transition hover:from-blue-600 hover:via-blue-700 hover:to-blue-800"
+            asChild
+          >
             <NavLink to="/signup">Sign Up</NavLink>
           </Button>
         </div>
