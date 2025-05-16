1a927a5273e19b8c7a003d1e6622d1329d40745c
msllagas <mndyllagas@gmail.com>
2025-05-16 22:05:57 +0800

    feat(home): add Home page and configure index route

---
+++ src/pages/dashboard/views/Home.tsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <>
+      <h1>This is Home - index page of Dashboard</h1>
+    </>
+  )
+}
+
+export { Home };
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,8 @@ import { createBrowserRouter } from "react-router";
 import { Welcome } from "@/pages/home/Welcome.tsx";
 import { Login } from "@/pages/auth/Login.tsx";
 import { SignUp } from "@/pages/auth/SignUp.tsx";
-import { Dashboard} from "@/pages/dashboard";
+import { Dashboard } from "@/pages/dashboard";
+import { Home } from "@/pages/dashboard/views";
 
 const router = createBrowserRouter([
   {
@@ -11,16 +12,17 @@ const router = createBrowserRouter([
   },
   {
     path: "login",
-    Component: Login
+    Component: Login,
   },
   {
     path: "signup",
-    Component: SignUp
+    Component: SignUp,
   },
   {
     path: "app",
-    Component: Dashboard
-  }
+    Component: Dashboard,
+    children: [{ index: true, Component: Home }],
+  },
 ]);
 
 export default router;
