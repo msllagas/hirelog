a167a368d2e76f540f29860837befe277bc0f23b
msllagas <mndyllagas@gmail.com>
2025-06-01 14:15:35 +0800

    chore(devtools): configure React Query DevTools

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,22 @@ import { RouterProvider } from "react-router";
 import router from "@/routes";
 import { ThemeProvider } from "@/contexts/theme.tsx";
 import { AuthProvider } from "@/contexts/auth.tsx";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <>
-      <AuthProvider>
-        <ThemeProvider defaultTheme="dark" storageKey="hirelog-ui-theme">
-          <RouterProvider router={router} />
-        </ThemeProvider>
-      </AuthProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ThemeProvider defaultTheme="dark" storageKey="hirelog-ui-theme">
+            <RouterProvider router={router} />
+            <ReactQueryDevtools initialIsOpen={false} />
+          </ThemeProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     </>
   );
 }
