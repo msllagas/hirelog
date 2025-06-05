84ec6b624cc3c572ef8d88ae67f06e78e6305216
msllagas <mndyllagas@gmail.com>
2025-06-05 22:19:06 +0800

    feat(ui): implement initial pagination functionality

--- src/api/job.ts
+++ src/api/job.ts
@@ -2,6 +2,7 @@ import axios from "@/config/axios";
 
 const API_ENDPOINT = "/job-applications";
 
-export async function index() {
-  return await axios.get(API_ENDPOINT);
+export async function index( queryString: string | null = null) {
+  const url = queryString ? `${API_ENDPOINT}/?${queryString}` : API_ENDPOINT;
+  return await axios.get(url);
 }
--- src/pages/dashboard/views/Application.tsx
+++ src/pages/dashboard/views/Application.tsx
@@ -2,21 +2,36 @@ import { useSearchParams } from "react-router";
 import { useQuery } from "@tanstack/react-query";
 import { JobApplication } from "@/types/job.ts";
 import { index } from "@/api/job.ts";
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
 
 function Application() {
-  const [searchParams] = useSearchParams();
-  const page = searchParams.get("page") ?? 1;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const page = Number(searchParams.get("page")) || 1;
+  const totalPages = 5;
 
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["jobs", page],
     queryFn: async () => {
-      const r = await index();
+      const r = await index(`page=${page}`);
       return r.data.data;
     },
     staleTime: 1000 * 60 * 5,
     retry: false,
     refetchOnWindowFocus: false,
   });
+
+  function goToPage(page: number) {
+    if (page < 1 || page > totalPages) return;
+    setSearchParams({ page: page.toString() });
+  }
 
   return (
     <>
@@ -38,6 +53,60 @@ function Application() {
           ))}
         </ul>
       )}
+      <div>
+        <Pagination>
+          <PaginationContent>
+            <PaginationItem>
+              <PaginationPrevious
+                href={`?page=${page - 1}`}
+                onClick={(e) => {
+                  if (page <= 1) {
+                    e.preventDefault(); // Stop navigation
+                    return;
+                  }
+                  e.preventDefault();
+                  goToPage(page - 1);
+                }}
+                className={page <= 1 ? "pointer-events-none opacity-50" : ""}
+              />
+            </PaginationItem>
+            {[...Array(totalPages)].map((_, i) => {
+              const p = i + 1;
+              return (
+                <PaginationItem key={p}>
+                  <PaginationLink
+                    href="#"
+                    isActive={p === page}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      goToPage(p);
+                    }}
+                  >
+                    {p}
+                  </PaginationLink>
+                </PaginationItem>
+              );
+            })}
+            <PaginationItem>
+              <PaginationEllipsis />
+            </PaginationItem>
+            <PaginationItem>
+              <PaginationNext
+                href={`?page=${page + 1}`}
+                onClick={(e) => {
+                  if (page >= totalPages) {
+                    e.preventDefault();
+                    return;
+                  }
+                  e.preventDefault();
+                  goToPage(page + 1);
+                }}
+                className={page >= totalPages ? "pointer-events-none opacity-50" : ""}
+              />
+            </PaginationItem>
+          </PaginationContent>
+        </Pagination>
+      </div>
     </>
   );
 }
