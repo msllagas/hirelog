645ab90433cf4435759f473ffe91ee2933e090db
msllagas <mndyllagas@gmail.com>
2025-06-11 22:10:32 +0800

    feat(ui): add toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { ThemeProvider } from "@/contexts/theme.tsx";
 import { AuthProvider } from "@/contexts/auth.tsx";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Toaster} from "@/components/ui/sonner.tsx";
 
 const queryClient = new QueryClient();
 
@@ -15,6 +16,11 @@ function App() {
           <ThemeProvider defaultTheme="dark" storageKey="hirelog-ui-theme">
             <RouterProvider router={router} />
             <ReactQueryDevtools initialIsOpen={false} />
+            <Toaster
+              mobileOffset={{ bottom: "16px" }}
+              position="top-right"
+              duration={2000}
+            />
           </ThemeProvider>
         </AuthProvider>
       </QueryClientProvider>
