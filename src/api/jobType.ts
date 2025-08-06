1188967fe385ab039f71b32db2a592a14f1fc562
msllagas <mndyllagas@gmail.com>
2025-08-06 20:50:31 +0800

    chore(api): scaffold job types and api methods

---
+++ src/api/jobType.ts
@@ -0,0 +1,20 @@
+import axios from "@/config/axios";
+
+const API_ENDPOINT = "/job-types";
+
+export async function index( queryString: string | null = null) {
+  const url = queryString ? `${API_ENDPOINT}/?${queryString}` : API_ENDPOINT;
+  return await axios.get(url);
+}
+
+export async function create() {
+  // todo -> implement create
+}
+
+export async function update() {
+  // todo -> implement update
+}
+
+export async function  destroy(jobApplicationID: number) {
+  return await axios.delete(`${API_ENDPOINT}/${jobApplicationID}`);
+}
---
+++ src/types/jobType.ts
@@ -0,0 +1,4 @@
+export type JobTypeResponse = {
+  id: number,
+  name: string,
+};
